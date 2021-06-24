a712389108dd5ed3f76a2581834eb94dd7d3c751
ChiangYintso <392711804@qq.com>
2021-06-24 10:19:12 +0800

    add ray traced image

--- bin/ray_traced_img.cpp
+++ bin/ray_traced_img.cpp
@@ -8,12 +8,31 @@
 
 #include <iostream>
 
-color ray_color(const Ray& r) {
-  vec3 unit_direction = unit_vector(r.direction());
-  auto t = 0.5*(unit_direction.y() + 1.0);
-  // blendedValue = (1−t) * startValue + t * endValue
-  return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.9, 0.3, 1.0);
+double hit_sphere(const point3& center, double radius, const Ray& r) {
+  vec3 oc = r.origin() - center;
+  auto a = r.direction().length_squared();
+  auto half_b = dot(oc, r.direction());
+  auto c = oc.length_squared() - radius*radius;
+  auto discriminant = half_b*half_b - a*c;
+
+  if (discriminant < 0) {
+	return -1.0;
+  } else {
+	return (-half_b - sqrt(discriminant) ) / a;
+  }
 }
+
+color ray_color(const Ray& r) {
+  auto t = hit_sphere(point3(0,0,-1), 0.5, r);
+  if (t > 0.0) {
+	vec3 N = unit_vector(r.at(t) - vec3(0,0,-1));
+	return 0.5*color(N.x()+1, N.y()+1, N.z()+1);
+  }
+  vec3 unit_direction = unit_vector(r.direction());
+  t = 0.5*(unit_direction.y() + 1.0);
+  return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
+}
+
 
 int main() {
 
